be9e807e42f0ae943eae06477cef34e71f6f92ac
Daniel Levi <daniel@sistemasconfitec.com.br>
2025-11-23 19:51:59 -0300

    fix: paths vercel

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -2,8 +2,8 @@ import "dotenv/config";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+  output: "standalone",
 };
 
 export default nextConfig;
